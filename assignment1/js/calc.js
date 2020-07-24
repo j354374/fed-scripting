1ded2bb3d90afbe9fc7c69b842112c4694a508d6
j354374 <68623010+j354374@users.noreply.github.com>
2020-07-24 20:07:08 +0800

    Update calc.js

--- assignment1/js/calc.js
+++ assignment1/js/calc.js
@@ -4,4 +4,8 @@ console.log("Hello");
 
 function echo(number) {
     alert("You entered: " + number + "!");
+    
+if(Number.isNaN(number)) num = 0; 
+    alert("That's not a number!");     
+    
 }
