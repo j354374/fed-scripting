d0b778122bc8acdbfb06b1c372f096a4c1e260e0
j354374 <68623010+j354374@users.noreply.github.com>
2020-07-25 08:57:31 +0800

    Update calc.js

--- assignment1/js/calc.js
+++ assignment1/js/calc.js
@@ -5,7 +5,6 @@ console.log("Hello");
 function echo(fancynumber) {
     alert("You entered: " + fancynumber + "!");
     
-if(Number.isNaN(number)) num = 1; 
-    alert("That's not a number!");     
+
     
 }
