b7bbd78a53661ea1cf920336b01353dc30d1d27a
j354374 <68623010+j354374@users.noreply.github.com>
2020-07-25 09:10:51 +0800

    Update calc.js

--- assignment1/js/calc.js
+++ assignment1/js/calc.js
@@ -5,6 +5,8 @@ console.log("Hello");
 function echo(fancynumber) {
     alert("You entered: " + fancynumber + "!");
     
+    if (typeof fancynumber !== 'number')
+   alert("That's not a number!");
 
     
 }
