f216a3286a4a6ced7c5161109d7f77f62c81585b
j354374 <68623010+j354374@users.noreply.github.com>
2020-11-25 11:42:37 +0800

    Add files via upload

--- assignment1/js/task1.js
+++ assignment1/js/task1.js
@@ -7,104 +7,110 @@ Calculate the square and cube of the number. A number squared is a number that i
 // Function called when the form is submitted.
 // Function performs the calculations and returns false.
 function onSubmit(event) {
-    event.preventDefault()
-    calculate()
+  event.preventDefault();
+  calculate();
 }
 
+function findFactorial(fancynumber) {
+
+  console.log("function findFactorial has started");
+  var num = fancynumber;
+  var fact = 1;
+
+  //loop till the iterator i equals to number
+  var i;
+  for (i = 1; i <= num; i++) {
+
+    //formula to calculate factorial is to
+    //multiply the iterator i value with fact value.
+    fact = fact * i;
+    console.log(i);
+    console.log(fact);
+
+    //put the below lines of code out of this 'for loop' to show only the total value
+    document.getElementById("factorial").value = fact;
+  } //end for loop
+} //end function
 
 function calculate() { //start function calculate
 
 
-    // For storing the factorial, squared and cubed results:
+  // For storing the factorial, squared and cubed results:
 
 
-    console.log("calc started.");
-    // Get references to the form value:
-    const f = document.forms.main;
-    var fancynumber = f.numInput.value;
+  console.log("calc started.");
+  // Get references to the form value:
+  const f = document.forms.main;
+  var fancynumber = f.numInput.value;
+  console.log(fancynumber);
+  alert("You entered: " + fancynumber + "!");
+  // Add validation here later!
+  if (isNaN(fancynumber) || fancynumber < 1 || fancynumber > 50) {
+    alert("Input is not valid!");
+    return;
+  } else {
+
+    console.log("else");
+    /* testing code */
+
+    // Calculate the factorial results:
+    //HINT: the factorial of 0 is 1.
+
+    var num = fancynumber;
+
+    findFactorial(num);
+
+
+  } // end else
+
+
+
+  findFactorial(fancynumber);
+
+
+  // Calculate the squared results:
+
+  function findSquare() { //start function
+    //const f = document.forms.main;
+    //var fancynumber = f.numInput.value;
     console.log(fancynumber);
-    alert("You entered: " + fancynumber + "!");
-    // Add validation here later!
-    if (isNaN(fancynumber) || fancynumber < 1 || fancynumber > 50) {
-        alert("Input is not valid!");
-        return;
-    } else {
+    console.log("findSquare function started");
+    var fancySquare = Math.pow(fancynumber, 2);
+    console.log(fancySquare);
+    document.getElementById("squared").value = fancySquare;
+  } //end function
 
-        console.log("else");
-        /* testing code */
-
-        // Calculate the factorial results:
-        //HINT: the factorial of 0 is 1.
-        function findFactorial(fancynumber) {
-
-            console.log("function findFactorial has started");
-            var num = fancynumber;
-            var fact = 1;
-
-            //loop till the iterator i equals to number
-            for (i = 1; i <= num; i++) {
-
-                //formula to calculate factorial is to
-                //multiply the iterator i value with fact value.
-                fact = fact * i;
-                console.log(i);
-                console.log(fact);
-
-                //put the below lines of code out of this 'for loop' to show only the total value
-                document.getElementById("factorial").value = fact;
-            } //end for loop
-        } //end else
-
-    } // end function
+  findSquare(f, fancynumber);
+  // Calculate the cubed results:
 
 
+  function findCubed() { //start function
+    //const f = document.forms.main;
+    //var fancynumber = f.numInput.value;
+    console.log(fancynumber);
+    console.log("findCubed function started");
+    var fancyCubed = Math.pow(fancynumber, 3);
+    console.log(fancyCubed);
+    document.getElementById("cubed").value = fancyCubed;
+  } //end function
 
-    findFactorial(fancynumber);
+  findCubed(f, fancynumber);
 
-
-    // Calculate the squared results:
-
-    function findSquare(fancynumber) { //start function
-      const f = document.forms.main;
-      var fancynumber = f.numInput.value;
-      console.log(fancynumber);
-        console.log("findSquare function started");
-        var fancySquare = Math.pow(fancynumber, 2);
-        console.log(fancySquare);
-        document.getElementById("squared").value = fancySquare;
-    } //end function
-
-    findSquare();
-    // Calculate the cubed results:
-
-
-    function findCubed(fancynumber) { //start function
-        const f = document.forms.main;
-        var fancynumber = f.numInput.value;
-        console.log(fancynumber);
-        console.log("findCubed function started");
-        var fancyCubed = Math.pow(fancynumber, 3);
-        console.log(fancyCubed);
-        document.getElementById("cubed").value = fancyCubed;
-    } //end function
-
-    findCubed();
-
-    // Return false to prevent submission:
-    //return false;
-    //event.preventDefault();
+  // Return false to prevent submission:
+  //return false;
+  //event.preventDefault();
 
 } // End of calculate() function.
 
 // Function called when the window has been loaded.
 // Function needs to add an event listener to the form.
 function init() {
-    'use strict';
-    // Add an event listener to the form:
+  'use strict';
+  // Add an event listener to the form:
 
-    //document.getElementById("calcBtn").addEventListener("click", calculate);
-    const f = document.forms.main; // <- html form
-    f.calcBtn.addEventListener("click", onSubmit); // <- button listener
+  //document.getElementById("calcBtn").addEventListener("click", calculate);
+  const f = document.forms.main; // <- html form
+  f.calcBtn.addEventListener("click", onSubmit); // <- button listener
 
 
 
